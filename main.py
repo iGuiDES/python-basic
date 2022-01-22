b1027e6f4c3ce90329b36f57431afe2f0a73f9c2
Alexandr Yatsenko <49126669+iGuiDES@users.noreply.github.com>
2022-01-22 20:40:53 +0000

    Structure update

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
-# import task_1
-# import task_2
-from home import home_1
+# from module1 import task_1
+# from module1 import task_1
+# from module2.home import home_1
+# from module2.home import home_2
+# from module2.home import home_3
+# from module2 import task_1
+# from module2 import task_2
+# from module2 import task_3
+# from module2 import task_4
+# from module2 import task_5
+# from module2 import task_6
+# from module2 import task_7
+from module2 import task_8
---
+++ module2/home/home_2.py
@@ -0,0 +1,5 @@
+name = input('Type your name: ')
+print('Hello ' + name)
+print('__________________________')
+sur_name = input('Ведите фамилию: ')
+print('Ваша фамилия - ' + sur_name);
