6d79b9e53eb33c063110d9eb4eb2c8998a34e992
Alexandr Yatsenko <49126669+iGuiDES@users.noreply.github.com>
2022-01-22 19:04:49 +0000

    practice home

---
+++ home/home_1.py
@@ -0,0 +1,10 @@
+my_color = 'Зелёный'
+sport = 'Бег'
+print('Меня зовут')
+name = 'Иван'
+print(name)
+print('Мой любимый цвет:')
+print(my_color)
+print('Мой любимый вид спорта:')
+sport = 'Soccer'
+print(sport)
--- main.py
+++ main.py
@@ -1,2 +1,3 @@
 # import task_1
-import task_2
+# import task_2
+from home import home_1
